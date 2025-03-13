e237d6f153177ef94420e840d05a0751e1b3a530
Singosirutonamikaze <yaodieudonnesingo60@gmail.com>
2025-03-13 18:08:04 +0000

    fin du defis

--- js/accueil.js
+++ js/accueil.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let menuBtn = document.getElementById("menubtn");
-    let sidebar = document.querySelector(".sidebar");
-    let textContent = document.querySelector(".main");
-    menuBtn.addEventListener("click", function() {
-        sidebar.classList.toggle("hidden");
-        textContent.classList.toggle("sidebar-hidden");
-    });
-})
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburgerBtn.addEventListener('click', () => {
+  navMenu.classList.toggle('mobile-menu-active');
+  hamburgerBtn.querySelector('i').classList.toggle('fa-times');
+  hamburgerBtn.querySelector('i').classList.toggle('fa-bars');
+});
+
+// Fermer le menu en cliquant à l'extérieur
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('.nav-menu') && !e.target.closest('.hamburger-btn')) {
+    navMenu.classList.remove('mobile-menu-active');
+    hamburgerBtn.querySelector('i').classList.add('fa-bars');
+    hamburgerBtn.querySelector('i').classList.remove('fa-times');
+  }
+});
